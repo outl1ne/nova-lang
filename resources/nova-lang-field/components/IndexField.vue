88cbbc4079a64456a11d41654416b95ffb2edb8f
KasparRosin <kaspar.rosin@tptlive.ee>
2019-11-08 10:25:48 +0200

    add Index & Detail vue components

---
+++ resources/nova-lang-field/components/IndexField.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <span>{{ field.value }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['resource', 'resourceName', 'resourceId', 'field'],
+};
+</script>
--- resources/nova-locale-field-form/nova-locale-field-form.js
+++
@@ -1,5 +0,0 @@
-import NovaLangStore from '../store/index';
-Nova.booting((Vue, router, store) => {
-  Vue.component('form-locale-field', require('./components/FormField'));
-  store.registerModule('nova-lang/nova-lang-storage', NovaLangStore);
-});
