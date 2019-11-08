14bb0050def00e5ef114e62aecf4a6db0d40c643
Kaspar Rosin <kaspar.rosin@tptlive.ee>
2019-11-08 10:30:33 +0200

    tab fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 let mix = require('laravel-mix');
 
-mix
-  .setPublicPath('dist')
+mix.setPublicPath('dist')
   .js('resources/js/tool.js', 'js')
   .js('resources/nova-lang-field/nova-lang-field.js', 'js');
